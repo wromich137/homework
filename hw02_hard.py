eff6d59e5249e249a21d4da20de62d9299fd9f72
wromich137 <borisov.artem1996@yandex.ru>
2019-02-19 12:45:09 +0300

    hw02

---
+++ hw02_hard.py
@@ -0,0 +1,19 @@
+
+#equation = "y = -12x + 11111140.2121"
+#x = 2.5
+
+#a = float(equation[4:equation.find("x")])
+#b = float(equation[equation.find("+ ")+2:])
+
+#y = a*x+b
+#print(y)
+
+#date = "01.11.1985"
+
+#dd = int(date[0:2])
+#mm = int(date[3:5])
+#yy = int(date[6:10])
+#print(dd)
+#print(mm)
+#print(yy)
+
